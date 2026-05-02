a01cf3f41fb9f64f639c3f0944af403398433112
skifli <121291719+skifli@users.noreply.github.com>
2026-05-02 16:05:55 +0100

    feat: switch back to stylix

--- users/programs/styles.nix
+++ users/programs/styles.nix
@@ -1,59 +1,140 @@
+# Partially based on code from https://github.com/Weathercold/nixfiles/blob/master/home/modules/services/scheduling/darkman.nix
+
 {
   commonHostVars,
+  config,
   hostVars,
-  inputs,
   lib,
+  inputs,
   pkgs,
   userVars,
   ...
 }:
 
-{
-  home-manager.users.${userVars.username} = {
-    programs.vogix = {
-      enable = true;
-      appearance = {
-        theme = "retro";
-        variant = "light";
+let
+  switch-hm-specialisation = spec: ''
+    hm_gens=$(${
+      if config.nix.package != null then config.nix.package else pkgs.nixVersions.latest
+    }/bin/nix-store -q --referrers ~/.local/state/nix/profiles/home-manager)
+    "$(${pkgs.toybox}/bin/find $hm_gens -maxdepth 1 -type d -name specialisation)/${spec}/activate"
+  '';
+
+  call-screen-transition = ''
+    export NIRI_SOCKET="''$(find /run/user/$(id -u) -name 'niri*.sock' 2>/dev/null | head -n 1)"
+    if [[ -n "$NIRI_SOCKET" ]]; then
+      echo "Using socket found at $NIRI_SOCKET"
+      ${lib.getExe config.programs.niri.package} msg action do-screen-transition
+    else
+      echo "Cannot find NIRI_SOCKET; skipping screen transition"
+    fi
+  '';
+
+  enableDarkmanModule =
+    { ... }:
+
+    {
+      services.darkman = {
+        enable = true;
+
+        settings = (
+          lib.mkMerge [
+            {
+              lat = hostVars.latitude;
+              lng = hostVars.longitude;
+              usegeoclue = false;
+
+              dbusserver = true;
+              portal = true;
+
+              lightModeScripts."00-switch-hm-specialisation" = switch-hm-specialisation "day";
+              darkModeScripts."00-switch-hm-specialisation" = switch-hm-specialisation "night";
+            }
+            (lib.mkIf (userVars.programs.compositor == "niri") {
+              lightModeScripts."01-call-screen-transition" = call-screen-transition;
+              darkModeScripts."01-call-screen-transition" = call-screen-transition;
+            })
+          ]
+        );
       };
     };
+in
+{
+  home-manager.users.${userVars.username} = {
+    stylix = {
+      enable = true;
+      base16Scheme = lib.mkDefault "${pkgs.base24-schemes}/share/themes/${commonHostVars.theme.day}.yaml";
 
-    # Theme cursor, fonts, and icons directly (vogix handles colors)
-    home.pointerCursor = {
-      package = commonHostVars.cursor.package;
-      size = commonHostVars.cursor.size;
-      name = commonHostVars.cursor.day.name;
-      x11.enable = true;
-      gtk.enable = true;
+      cursor = {
+        package = commonHostVars.cursor.package;
+        size = commonHostVars.cursor.size;
+        name = lib.mkDefault commonHostVars.cursor.day.name;
+      };
+      icons = commonHostVars.icons;
+      fonts = commonHostVars.fonts;
+
+      # targets."${userVars.programs.browser}-browser".enable = false;
     };
 
     gtk = {
       enable = true;
 
-      font = {
-        package = commonHostVars.fonts.sansSerif.package;
-        name = commonHostVars.fonts.sansSerif.name;
-        size = commonHostVars.fonts.sizes.applications;
-      };
-
       iconTheme = {
         package = commonHostVars.icons.package;
-        name = commonHostVars.icons.light;
-      };
-    };
-
-    qt = {
-      enable = true;
-
-      platformTheme.name = "gtk3";
-      style = {
-        package = commonHostVars.icons.package;
-        name = "gtk2";
+        name = lib.mkDefault commonHostVars.icons.light;
       };
     };
   };
 
+  specialisation = {
+    day.configuration =
+      {
+        pkgs,
+        ...
+      }:
+      let
+        name = "day";
+      in
+      {
+        # xdg.dataFile."home-manager/specialisation".text = name;
+        system.nixos.tags = [ name ];
+        environment.etc."specialisation".text = name;
+
+        home-manager.users.${userVars.username} = {
+          gtk.iconTheme.name = commonHostVars.icons.light;
+
+          stylix = {
+            base16Scheme = "${pkgs.base24-schemes}/share/themes/${commonHostVars.theme.day}.yaml";
+            cursor.name = commonHostVars.cursor.day.name;
+          };
+        };
+      };
+
+    night.configuration =
+      {
+        pkgs,
+        ...
+      }:
+      let
+        name = "night";
+      in
+      {
+        # xdg.dataFile."home-manager/specialisation".text = name;
+        system.nixos.tags = [ name ];
+        environment.etc."specialisation".text = name;
+
+        home-manager.users.${userVars.username} = {
+          gtk.iconTheme.name = commonHostVars.icons.dark;
+
+          stylix = {
+            base16Scheme = "${pkgs.base24-schemes}/share/themes/${commonHostVars.theme.night}.yaml";
+            cursor.name = commonHostVars.cursor.night.name;
+          };
+        };
+      };
+  };
+
   home-manager.sharedModules = [
-    inputs.vogix.homeManagerModules.default
+    enableDarkmanModule
+    inputs.stylix.homeModules.stylix
   ];
 }
