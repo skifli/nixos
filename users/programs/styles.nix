76fd85d11c83213249aaf3c23cf3141b8a650021
skifli <121291719+skifli@users.noreply.github.com>
2026-05-02 14:43:10 +0100

    fix: `styles.nix`

--- users/programs/styles.nix
+++ users/programs/styles.nix
@@ -10,7 +10,6 @@
 
 {
   home-manager.users.${userVars.username} = {
-    # Runtime color switching with vogix for instant day/night transitions
     programs.vogix = {
       enable = true;
       appearance = {
@@ -19,31 +18,42 @@
       };
     };
 
-    stylix = {
-      enable = true;
-      autoEnable = false;
-
-      cursor = {
-        package = commonHostVars.cursor.package;
-        size = commonHostVars.cursor.size;
-        name = commonHostVars.cursor.day.name;
-      };
-      icons = commonHostVars.icons;
-      fonts = commonHostVars.fonts;
+    # Theme cursor, fonts, and icons directly (vogix handles colors)
+    home.pointerCursor = {
+      package = commonHostVars.cursor.package;
+      size = commonHostVars.cursor.size;
+      name = commonHostVars.cursor.day.name;
+      x11.enable = true;
+      gtk.enable = true;
     };
 
     gtk = {
       enable = true;
+
+      font = {
+        package = commonHostVars.fonts.sansSerif.package;
+        name = commonHostVars.fonts.sansSerif.name;
+        size = commonHostVars.fonts.sizes.applications;
+      };
 
       iconTheme = {
         package = commonHostVars.icons.package;
         name = commonHostVars.icons.light;
       };
     };
+
+    qt = {
+      enable = true;
+
+      platformTheme.name = "gtk3";
+      style = {
+        package = commonHostVars.icons.package;
+        name = "gtk2";
+      };
+    };
   };
 
   home-manager.sharedModules = [
     inputs.vogix.homeManagerModules.default
-    inputs.stylix.homeModules.stylix
   ];
 }
