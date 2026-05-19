8e0a16038ab5554e284e5b2a23bb5ec1938bb139
skifli <121291719+skifli@users.noreply.github.com>
2026-05-19 11:06:23 +0100

    feat: update printing and remove hash for updated awatcher

--- modules/core/printing.nix
+++ modules/core/printing.nix
@@ -1,8 +1,20 @@
-_: {
+{pkgs, ...}: {
   services = {
     printing = {
       enable = true;
-      drivers = [];
+      drivers = with pkgs; [
+        splix
+      ];
+    };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    system-config-printer
+  ];
 }
