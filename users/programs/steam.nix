f22a49434266fa4de95dbb106d77897d123443d6
skifli <121291719+skifli@users.noreply.github.com>
2026-06-03 20:45:44 +0100

    fix: wrong placement of proton-ge-bin

--- users/programs/steam.nix
+++ users/programs/steam.nix
@@ -5,7 +5,6 @@
 }: {
   home-manager.users.${userVars.username} = {
     home.packages = with pkgs; [
-      proton-ge-bin
       protontricks
       gamescope
       mangohud
@@ -17,5 +16,10 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
+    # Safely install and register Proton-GE for Steam
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 }
