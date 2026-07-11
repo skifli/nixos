afa81ceb6147caaf469cedb425c45426dfe64bca
skifli <121291719+skifli@users.noreply.github.com>
2026-07-11 12:16:48 +0100

    fix: try move dolphinrc to activation

--- users/programs/explorer-gui/dolphin.nix
+++ users/programs/explorer-gui/dolphin.nix
@@ -5,42 +5,75 @@
   ...
 }: {
   /*
-     nixpkgs.overlays = [
+  nixpkgs.overlays = [
     inputs.dolphin-overlay.overlays.default # Not working - TODO: Fix myself later!?
   ]; # Add https://github.com/MattiDragon/dolphin-overlay
   */
 
-  environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu"; # TODO: 3REMOVE AFTER https://github.com/NixOS/nixpkgs/issues/409986 IS SOLVED
+  environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu"; # TODO: REMOVE AFTER https://github.com/NixOS/nixpkgs/issues/409986 IS SOLVED
 
-  home-manager.users.${userVars.username} = {
-    home.packages = with pkgs; [
-      kdePackages.dolphin
-      kdePackages.dolphin-plugins
+  home-manager.users.${userVars.username} = { config, lib, ... }: {
+    home = {
+      packages = with pkgs; [
+        kdePackages.dolphin
+        kdePackages.dolphin-plugins
 
-      kdePackages.qtsvg # Support for svg icons
-      kdePackages.kio # Below ig, custom though
-      kdePackages.kio-admin # Another custom
-      kdePackages.kio-fuse # To mount remote filesystems via FUSE
-      kdePackages.kio-extras # Extra protocols support (sftp, fish and more)
+        kdePackages.qtsvg # Support for svg icons
+        kdePackages.kio # Below ig, custom though
+        kdePackages.kio-admin # Another custom
+        kdePackages.kio-fuse # To mount remote filesystems via FUSE
+        kdePackages.kio-extras # Extra protocols support (sftp, fish and more)
 
-      # File previes - https://wiki.archlinux.org/title/Dolphin#File_previews
-      libappimage
-      libheif
-      icoutils
-      kdePackages.ark # File extraction
-      kdePackages.ffmpegthumbs
-      kdePackages.kactivitymanagerd
-      kdePackages.kdegraphics-thumbnailers
-      kdePackages.kimageformats
-      kdePackages.kdesdk-thumbnailers
-      kdePackages.kservice # For kbuildsycoca6
-      kdePackages.qtimageformats
-      nufraw-thumbnailer # Own choice
-      resvg
-      taglib_1
+        # File previews - https://wiki.archlinux.org/title/Dolphin#File_previews
+        libappimage
+        libheif
+        icoutils
+        kdePackages.ark # File extraction
+        kdePackages.ffmpegthumbs
+        kdePackages.kactivitymanagerd
+        kdePackages.kdegraphics-thumbnailers
+        kdePackages.kimageformats
+        kdePackages.kdesdk-thumbnailers
+        kdePackages.kservice # For kbuildsycoca6
+        kdePackages.qtimageformats
+        nufraw-thumbnailer # Own choice
+        resvg
+        taglib_1
 
-      unzip
-    ];
+        unzip
+      ];
+
+      activation.setupDolphinrc = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        TARGET_FILE="${config.xdg.configHome}/dolphinrc"
+        
+        if [ ! -f "$TARGET_FILE" ]; then
+          mkdir -p "$(dirname "$TARGET_FILE")"
+          cat << 'EOF' > "$TARGET_FILE"
+MenuBar=Disabled
+
+[General]
+ShowFullPathInTitlebar=true
+Version=202
+ViewPropsTimestamp=2026,7,9,16,52,45.737
+
+[KFileDialog Settings]
+Places Icons Auto-resize=true
+Places Icons Static Size=0
+
+[MainWindow]
+MenuBar=Disabled
+
+[PreviewSettings]
+EnableRemoteFolderThumbnail=true
+MaximumRemoteSize=104857600
+Plugins=imagethumbnail,jpegthumbnail,directorythumbnail,ffmpegthumbs,exethumbnail,comicbookthumbnail,officeMarcothumbnail
+RemotePlugins=imagethumbnail,jpegthumbnail,directorythumbnail,ffmpegthumbs,exethumbnail,comicbookthumbnail,officeMarcothumbnail
+RemotePreviewSizeLimit=104857600
+UseDefaultRemotePreviewSizeLimit=false
+EOF
+        fi
+      '';
+    };
 
     xdg.mimeApps.defaultApplications = {
       "inode/directory" = ["dolphin.desktop"];
@@ -77,28 +110,5 @@
         else userVars.programs.terminal
       }.desktop
     '';
-
-    xdg.configFile."dolphinrc".text = ''
-      MenuBar=Disabled
-
-      [General]
-      ShowFullPathInTitlebar=true
-      Version=202
-      ViewPropsTimestamp=2026,7,9,16,52,45.737
-
-      [KFileDialog Settings]
-      Places Icons Auto-resize=false
-      Places Icons Static Size=22
-
-      [MainWindow]
-      MenuBar=Disabled
-
-      [PreviewSettings]
-      EnableRemoteFolderThumbnail=true
-      MaximumRemoteSize=104857600
-      Plugins=imagethumbnail,jpegthumbnail,directorythumbnail,ffmpegthumbs,exethumbnail,comicbookthumbnail,officeMarcothumbnail
-      RemotePlugins=imagethumbnail,jpegthumbnail,directorythumbnail,ffmpegthumbs,exethumbnail,comicbookthumbnail,officeMarcothumbnail
-      RemotePreviewSizeLimit=104857600
-      UseDefaultRemotePreviewSizeLimit=false'';
   };
 }
