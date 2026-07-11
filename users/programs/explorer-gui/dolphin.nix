ef60fd092411a1d3ff910c00db43f48caab186ca
skifli <121291719+skifli@users.noreply.github.com>
2026-07-11 10:47:48 +0000

    chore(fmt): run alejandra

--- users/programs/explorer-gui/dolphin.nix
+++ users/programs/explorer-gui/dolphin.nix
@@ -4,9 +4,11 @@
   userVars,
   ...
 }: {
-  /* nixpkgs.overlays = [
+  /*
+     nixpkgs.overlays = [
     inputs.dolphin-overlay.overlays.default # Not working - TODO: Fix myself later!?
-  ]; # Add https://github.com/MattiDragon/dolphin-overlay */
+  ]; # Add https://github.com/MattiDragon/dolphin-overlay
+  */
 
   environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu"; # TODO: 3REMOVE AFTER https://github.com/NixOS/nixpkgs/issues/409986 IS SOLVED
 
