992f92d9f7aacd9f1f874d979853eb87e0c98300
skifli <121291719+skifli@users.noreply.github.com>
2026-07-13 12:24:14 +0100

    fix: set pdfs to evince

--- users/ami/user-packages.nix
+++ users/ami/user-packages.nix
@@ -3,37 +3,43 @@
   userVars,
   ...
 }: {
-  home-manager.users.${userVars.username}.home.packages = with pkgs; [
-    anytype
-    cameractrls
-    cameractrls-gtk4
-    # cherry-studio # This, kicad, and 1 below removed due to the builders not already having it and rebuilding was taking forever
-    # davinci-resolve #
-    droidcam
-    evince # Document viewer
-    ferdium
-    fira-code
-    haruna # Media player
-    hblock
-    imagemagick # mogrift EXIF
-    kdePackages.gwenview # Image viewer
-    # kdePackages.kate # Also installs kwrite
-    kdePackages.qtwayland # Needed by typstwriter
-    # kicad
-    normcap
-    noto-fonts
-    noto-fonts-color-emoji
-    prismlauncher
-    javaPackages.compiler.temurin-bin.jdk-25 # For Prism Launcher
-    proton-vpn
-    qtscrcpy
-    remmina
-    scrcpy
-    soundwireserver
-    # siyuan
+  home-manager.users.${userVars.username} = {
+    home.packages = with pkgs; [
+      anytype
+      cameractrls
+      cameractrls-gtk4
+      # cherry-studio # This, kicad, and 1 below removed due to the builders not already having it and rebuilding was taking forever
+      # davinci-resolve #
+      droidcam
+      evince # Document viewer
+      ferdium
+      fira-code
+      haruna # Media player
+      hblock
+      imagemagick # mogrify EXIF
+      kdePackages.gwenview # Image viewer
+      # kdePackages.kate # Also installs kwrite
+      kdePackages.qtwayland # Needed by typstwriter
+      # kicad
+      normcap
+      noto-fonts
+      noto-fonts-color-emoji
+      prismlauncher
+      javaPackages.compiler.temurin-bin.jdk-25 # For Prism Launcher
+      proton-vpn
+      qtscrcpy
+      remmina
+      scrcpy
+      soundwireserver
+      # siyuan
 
-    mpv
-  ];
+      mpv
+    ];
+
+    xdg.mimeApps.defaultApplications = {
+      "application/pdf" = "org.gnome.Evince.desktop";
+    };
+  }
 
   # `programs.adb` is deprecated in newer NixOS (systemd 258+ handles uaccess)
   # Install `adb` via `pkgs.android-tools` at the system level instead.
