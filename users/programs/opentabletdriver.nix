5445e4620809f17b85f2c6a605fdb768abfe198f
skifli <121291719+skifli@users.noreply.github.com>
2026-07-16 23:39:31 +0100

    fix: otd on startup crash

--- users/programs/opentabletdriver.nix
+++ users/programs/opentabletdriver.nix
@@ -12,6 +12,13 @@
   # Enable OpenTabletDriver
   hardware.opentabletdriver.enable = true;
 
+  systemd.user.services.opentabletdriver = {
+    Unit = {
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+  }; # Till https://github.com/OpenTabletDriver/OpenTabletDriver/issues/4885 is resolved
+
   # Required by OpenTabletDriver
   hardware.uinput.enable = true;
   boot.kernelModules = ["uinput"];
