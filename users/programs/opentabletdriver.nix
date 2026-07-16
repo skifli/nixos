f0754ab178b7a6164269b0414d351ee469e893f2
skifli <121291719+skifli@users.noreply.github.com>
2026-07-16 23:46:55 +0100

    fix: root level services don't use Unit

--- users/programs/opentabletdriver.nix
+++ users/programs/opentabletdriver.nix
@@ -13,11 +13,10 @@
   hardware.opentabletdriver.enable = true;
 
   systemd.user.services.opentabletdriver = {
-    Unit = {
-      After = ["graphical-session.target"];
-      PartOf = ["graphical-session.target"];
-    };
-  }; # Till https://github.com/OpenTabletDriver/OpenTabletDriver/issues/4885 is resolved
+    after = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+  };
+  # Till https://github.com/OpenTabletDriver/OpenTabletDriver/issues/4885 is resolved
 
   # Required by OpenTabletDriver
   hardware.uinput.enable = true;
