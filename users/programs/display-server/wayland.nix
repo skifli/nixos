db10812966741b38246242ad75fb390241f723b3
skifli <121291719+skifli@users.noreply.github.com>
2026-07-18 19:16:28 +0100

    fix: old env vars not needed

--- users/programs/display-server/wayland.nix
+++ users/programs/display-server/wayland.nix
@@ -7,18 +7,12 @@
   # Set Wayland-friendly environment variables
   environment.sessionVariables = {
     APP2UNIT_SLICES = "a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice"; # https://github.com/Vladimir-csp/app2unit?tab=readme-ov-file#uwsm-integration
-    CLUTTER_BACKEND = "wayland";
-    EGL_PLATFORM = "wayland";
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    GDK_BACKEND = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
     NIXOS_OZONE_WL = "1";
-    OZONE_PLATFORM = "wayland";
     # QT_QPA_PLATFORM = "wayland"; # Not needed - https://discourse.nixos.org/t/davinci-resolve-only-launches-as-root/54258/6
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     QT_WAYLAND_SHELL_INTEGRATION = "xdg-shell";
     SDL_VIDEODRIVER = "wayland";
-    XDG_SESSION_TYPE = "wayland";
   };
 
   # Enable fundamental Wayland utilities and portals
