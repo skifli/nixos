4676b4058c1ee004e5d0aaf0bb84a149a8ca4783
skifli <121291719+skifli@users.noreply.github.com>
2026-07-19 16:28:22 +0100

    fix: pkgs.lib not lib

--- users/programs/remote-desktop/remmina.nix
+++ users/programs/remote-desktop/remmina.nix
@@ -9,7 +9,7 @@
         remmina
       ];
 
-      home.activation.setRemminaKeyboard = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      home.activation.setRemminaKeyboard = pkgs.lib.hm.dag.entryAfter ["writeBoundary"] ''
         # Make it use the client keymap
         sed -i 's/^rdp_use_client_keymap.*/rdp_use_client_keymap=1/' "$HOME/.config/remmina/remmina.pref"
 
