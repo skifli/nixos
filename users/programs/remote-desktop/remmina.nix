a613d41d050c6a54833c91196abf5cab32411aa5
skifli <121291719+skifli@users.noreply.github.com>
2026-07-19 15:26:48 +0000

    chore(fmt): run alejandra

--- users/programs/remote-desktop/remmina.nix
+++ users/programs/remote-desktop/remmina.nix
@@ -10,12 +10,12 @@
       ];
 
       home.activation.setRemminaKeyboard = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  # Make it use the client keymap
-  sed -i 's/^rdp_use_client_keymap.*/rdp_use_client_keymap=1/' "$HOME/.config/remmina/remmina.pref"
-  
-  # Don't passthrough Windows / mod key
-  sed -i 's/^rdp_kbd_remap.*/rdp_kbd_remap=0x15b=/' "$HOME/.config/remmina/remmina.pref"
-'';
+        # Make it use the client keymap
+        sed -i 's/^rdp_use_client_keymap.*/rdp_use_client_keymap=1/' "$HOME/.config/remmina/remmina.pref"
+
+        # Don't passthrough Windows / mod key
+        sed -i 's/^rdp_kbd_remap.*/rdp_kbd_remap=0x15b=/' "$HOME/.config/remmina/remmina.pref"
+      '';
     };
   };
 }
