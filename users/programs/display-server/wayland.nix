35b47c0fb56ccb91f4cd7ad5d0f758d80326e595
skifli <121291719+skifli@users.noreply.github.com>
2026-07-19 10:56:05 +0100

    fix: need `kdePackages.` behind pkg names

--- users/programs/display-server/wayland.nix
+++ users/programs/display-server/wayland.nix
@@ -25,9 +25,9 @@
 
     slurp # https://wiki.archlinux.org/title/XDG_Desktop_Portal#Using_multiple_monitors_with_xdg-desktop-portal-wlr
 
-    kwallet # Provides helper service
-    kwallet-pam # PRovides helper service
-    kwalletmanager # Provids KCMs and stuff
+    kdePackages.kwallet # Provides helper service
+    kdePackages.kwallet-pam # PRovides helper service
+    kdePackages.kwalletmanager # Provids KCMs and stuff
   ];
 
   # Enable XWayland support system-wide
