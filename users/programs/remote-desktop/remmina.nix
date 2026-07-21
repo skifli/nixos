f5e73b4ab53f2031a766fe0cab6066ed03896f29
skifli <121291719+skifli@users.noreply.github.com>
2026-07-21 22:05:31 +0100

    feat: add wayle panel toggle bind + switch remmina to change pref for keyboard layout not use client keymap

--- users/programs/remote-desktop/remmina.nix
+++ users/programs/remote-desktop/remmina.nix
@@ -1,9 +1,20 @@
 {
+  hostVars,
   lib,
   pkgs,
   userVars,
   ...
-}: {
+}: let
+  # Map standard Linux keyboard layouts to Remmina's RDP LCID decimal codes
+  rdpLayoutMap = {
+    "gb" = "809";
+    "uk" = "809";
+    "us" = "409";
+  };
+  
+  # Look up hostVars.keyboardLayout
+  rdpCode = rdpLayoutMap.${hostVars.keyboardLayout};
+in {
   home-manager.users.${userVars.username} = {lib, ...}: {
     home = {
       packages = with pkgs; [
@@ -11,8 +22,8 @@
       ];
 
       activation.setRemminaKeyboard = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        # Make it use the client keymap
-        sed -i 's/^rdp_use_client_keymap.*/rdp_use_client_keymap=1/' "$HOME/.config/remmina/remmina.pref"
+        # Make it use the desired keyboard layout
+        sed -i 's/^rdp_keyboard_layout.*/rdp_keyboard_layout=${rdpCode}/' "$HOME/.config/remmina/remmina.pref"
 
         # Don't passthrough Windows / mod key
         sed -i 's/^rdp_kbd_remap.*/rdp_kbd_remap=0x15b=/' "$HOME/.config/remmina/remmina.pref"
