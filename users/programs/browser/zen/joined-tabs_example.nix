99e139b2606a3f5464d3297b01591608dc2d2814
skifli <121291719+skifli@users.noreply.github.com>
2026-07-22 17:46:21 +0100

    feat(zen): add joinedTabs parentId option (0xc000022070/zen-browser-flake@86ec039)

--- users/programs/browser/zen/joined-tabs_example.nix
+++ users/programs/browser/zen/joined-tabs_example.nix
@@ -13,6 +13,7 @@ in
   mkJoinedTabs {
     "Example joined tabs vertical" = {
       id = "example-joined-tabs";
+      folderParentId = pins.folderSpecs."Some folder".id;
       gridType = "vsep";
       tabs = [
         "Joined Example Tab 1"
@@ -22,6 +23,7 @@ in
 
     "Example joined tabs horizontal" = {
       id = "example-joined-tabs-hesp";
+      folderParentId = pins.folderSpecs."Some folder".id;
       gridType = "hsep";
       tabs = [
         "Joined Example Tab 3"
