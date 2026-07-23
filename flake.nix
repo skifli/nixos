f29ad73e7f434a46bcb7a625a39da74474d48a92
skifli <121291719+skifli@users.noreply.github.com>
2026-07-23 15:00:13 +0000

    chore(fmt): run alejandra

--- flake.nix
+++ flake.nix
@@ -74,14 +74,16 @@
           modules = [
             ./hosts/${hostname}/configuration.nix
 
-             ({ pkgs, ... }: {
+            ({pkgs, ...}: {
               nixpkgs.overlays = [
                 (final: prev: {
-                  inherit (prev.lixPackageSets.stable)
+                  inherit
+                    (prev.lixPackageSets.stable)
                     nixpkgs-review
                     nix-eval-jobs
                     nix-fast-build
-                    colmena;
+                    colmena
+                    ;
                 })
               ];
 
