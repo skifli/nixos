31848044b9a15901e9949f855c7bbbea2749a47c
skifli <121291719+skifli@users.noreply.github.com>
2026-07-23 18:43:44 +0100

    fix(safeeyes): add optional dependencies into deps

--- users/programs/ergonomics/safeeyes.nix
+++ users/programs/ergonomics/safeeyes.nix
@@ -4,6 +4,14 @@
   ...
 }: {
   home-manager.users.${userVars.username} = {
-    home.packages = with pkgs; [safeeyes];
+    home.packages = with pkgs; [
+      (safeeyes.overridePythonAttrs (oldAttrs: {
+        # The following are for some reason defined in the https://github.com/NixOS/nixpkgs/blob/b3fe9581c9061c749abef42b6d4ee7b7c05c33fa/pkgs/by-name/sa/safeeyes/package.nix#L52 file as optional-dependencies but not actual dependencies, no idea why as optional-dependencies is not as far as I am aware reference further (at least in that file), but anyway. Maybe I don't understand how optional-dependencies should work
+        dependencies = (oldAttrs.dependencies or []) ++ [
+          python3Packages.croniter # Needed for Health Statistics
+          python3Packages.pywayland # Needed for Smart Pause
+        ];
+      }))
+    ];
   };
 }
