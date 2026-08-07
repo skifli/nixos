df86b074348311c274f8703c04b740dc1746d362
skifli <121291719+skifli@users.noreply.github.com>
2026-08-07 23:34:20 +0100

    fix(sunsetr): auto-update conf to homeVars lat/long

--- users/programs/nightlight/sunsetr.nix
+++ users/programs/nightlight/sunsetr.nix
@@ -7,5 +7,10 @@
     home.packages = with pkgs; [
       sunsetr
     ];
+
+    home.activation.setSunsetrCoordinates = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      sed -i 's/^latitude.*/latitude=${hostVars.latitude}/' "$HOME/.config/sunsetr/sunsetr.toml"
+      sed -i 's/^longitude.*/longitude=${hostVars.longitude}/' "$HOME/.config/sunsetr/sunsetr.toml"
+    '';
   };
 }
