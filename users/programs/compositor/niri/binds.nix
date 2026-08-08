be133e480dfc16dcc84d068a8cc79490a8c288fa
skifli <121291719+skifli@users.noreply.github.com>
2026-08-08 15:38:55 +0100

    feat(nirius): use niri logo in notification + beautify notif in general

--- users/programs/compositor/niri/binds.nix
+++ users/programs/compositor/niri/binds.nix
@@ -351,7 +351,8 @@ with config.home-manager.users.${userVars.username}.lib.niri.actions; {
 
   # NIRIUS - MARKS (T = Tag)
   "Mod+T" = {
-    action = spawn "sh" "-c" "nirius toggle-mark && list=\$(nirius list-marked | awk -F', ' 'NF{for(i=1;i<=NF;i++){split(\$i,a,\": \"); gsub(/Some\\(\"|\"\\)|Some\\(|\\)/,\"\",a[2]); m[a[1]]=a[2]} print m[\"on workspace\"] \"\\t• \" m[\"app-id\"] \" — \" m[\"title\"] \" (WS \" m[\"on workspace\"] \")\"}' | sort -n | cut -f2-) && if [ -z \"\$list\" ]; then list='No windows marked'; fi && printf '%s\\n' \"\$list\" && notify-send 'Marked Windows' \"\$list\"";
+    action = spawn "sh" "-c" "nirius toggle-mark && list=\$(nirius list-marked | awk -F', ' 'NF{for(i=1;i<=NF;i++){split(\$i,a,\": \"); gsub(/Some\\(\"|\"\\)|Some\\(|\\)/,\"\",a[2]); m[a[1]]=a[2]} print m[\"on workspace\"] \"\\t• \" m[\"app-id\"] \" — \" m[\"title\"] \" (WS \" m[\"on workspace\"] \")\"}' | sort -n | cut -f2-) && if [ -z \"\$list\" ]; then list='No windows marked'; fi && printf '%s\\n' \"\$list\" && notify-send -e -a nirius -i /home/${userVars.username}/.local/share/misc/niri-icon.svg 'Marked Windows' \"\$list\"";
+    # -e transient, -a app name, -i icon
     hotkey-overlay.title = "Tag/untag window";
   };
   "Mod+Shift+T" = {
