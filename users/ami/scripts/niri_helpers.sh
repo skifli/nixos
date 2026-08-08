10c292881afc4655deff5519a4b9cccf9b66027e
skifli <121291719+skifli@users.noreply.github.com>
2026-08-08 13:44:43 +0100

    feat(niri): start using nirius

--- users/ami/scripts/niri_helpers.sh
+++ users/ami/scripts/niri_helpers.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO - Integrate https://sr.ht/~tsdh/nirius for better technical debt
+
 export PATH="$PATH:/run/current-system/sw/bin:$HOME/.nix-profile/bin"
 
 export MON_1="HDMI-A-1"
