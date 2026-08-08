111e57fe6994a4e382c7c194217adfa25579bef3
skifli <121291719+skifli@users.noreply.github.com>
2026-08-08 15:51:19 +0100

    feat(niri): update scripts to use `notify-send`

--- users/ami/scripts/3-niri.sh
+++ users/ami/scripts/3-niri.sh
@@ -2,11 +2,11 @@
 
 source "${BASH_SOURCE%/*}/niri_helpers.sh"
 
-echo "Initing 'f1is' window positions..."
+notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" "F1iS configuration" "Initing window positions"
 
 ensure_window_exists "affinity.exe" "" "affinity-v3" "wineboot.exe" "Wine"
 
-echo "Restoring 'f1is' window positions..."
+notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" "F1iS configuration" "Restoring window positions"
 
 # 1st Monitor
 move_windows "app_id" "zen-beta" "$MON_1" "1" "100%"
@@ -31,4 +31,4 @@ focus_window "app_id" "wineboot.exe" # Affinity booting-up
 focus_window "app_id" "affinity.exe"
 focus_window "app_id" "gcr-prompter"
 
-echo "Window rearrangement complete!"
+notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" "F1iS configuration" "Window positions restored"
