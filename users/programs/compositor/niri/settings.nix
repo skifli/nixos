1c399d2478aaf8ce40df195aebaf92fd1855c4e6
skifli <121291719+skifli@users.noreply.github.com>
2026-08-09 22:19:02 +0100

    feat(niri): add awww to backdrop, add vrr to outputs, remove touchpad from inputs, increase `max-scroll-amount` to 10%

--- users/programs/compositor/niri/settings.nix
+++ users/programs/compositor/niri/settings.nix
@@ -15,6 +15,7 @@
       _args = [name];
       inherit (hostVars.outputs.${name}) mode scale;
       position._props = hostVars.outputs.${name}.position;
+      variable-refresh-rate._props = { on-demand = "true"; };
     }
     // (pkgs.lib.optionalAttrs (hostVars.outputs.${name}.focus-at-startup or false) {
       focus-at-startup = [];
