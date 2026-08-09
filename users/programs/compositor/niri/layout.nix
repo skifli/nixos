384a087f9270ed94af10e0add9162b7fd8a5d9fb
skifli <121291719+skifli@users.noreply.github.com>
2026-08-09 22:56:24 +0100

    feat(niri): update layout configuration + add more cycling hotkeys

--- users/programs/compositor/niri/layout.nix
+++ users/programs/compositor/niri/layout.nix
@@ -1,8 +1,9 @@
 {...}: {
   gaps = 0;
   center-focused-column = "on-overflow";
-  always-center-single-column = [];
+  # always-center-single-column = [];
   empty-workspace-above-first = false;
+  default-column-display = "normal";
 
   preset-column-widths._children = [
     {proportion = 0.25;}
@@ -12,23 +13,35 @@
     {proportion = 0.75;}
   ];
 
-  # Disables window border
-  border.off = [];
+  # This is a bit unclearly defined in the Wayland protocol, so some clients may misinterpret it. Either way, default-column-width {} is most useful for specific windows, in form of a window rule with the same syntax.
+  default-column-width = {};
+
+  preset-window-heights._children = [
+    {proportion = 0.25;}
+    {proportion = 1.0 / 3.0;}
+    {proportion = 0.5;}
+    {proportion = 2.0 / 3.0;}
+    {proportion = 0.75;}
+  ];
 
   # Disables focus ring
   focus-ring.off = [];
 
-  # Enables window shadow
-  shadow = {
-    draw-behind-window = true;
-    softness = 20;
-    spread = 5;
-    offset._props = {
-      x = 5;
-      y = 5;
-    };
-    color = "#000000aa";
+  # Disables window border
+  border.off = [];
+
+  # Disables window shadow (manually enabled for floating windows in window-rules.nix)
+  shadow.off = [];
+
+  # Enables the tab indicator
+  tab-indicator = {
+    on = [];
+    position = "top";
+    hide-when-single-tab = [];
   };
+
+  # Enables the insert hint
+  insert-hint.on = [];
 
   # Screen edge struts
   struts = {
@@ -36,9 +49,5 @@
     left = 0;
     right = 0;
     bottom = 0;
-  };
-
-  tab-indicator = {
-    position = "top";
   };
 }
