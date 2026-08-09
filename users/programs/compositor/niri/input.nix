b99d9b36f927d1dfdfd6879032b0b5c55c7288b8
skifli <121291719+skifli@users.noreply.github.com>
2026-08-09 20:42:19 +0100

    fix(niri): init browser blurring?

--- users/programs/compositor/niri/input.nix
+++ users/programs/compositor/niri/input.nix
@@ -26,11 +26,13 @@
     natural-scroll = [];
   };
 
-  # Enables mouse warping to center of newly focused window
-  warp-mouse-to-focus = [];
-
+  disable-power-key-handling = []; # By default niri takes over the power button and makes it sleep not power off
+  warp-mouse-to-focus._props = {
+    mode = "center-xy"; # Warps by both X and Y together, so won't warp if it was already somewhere inside the newly focused window
+  }; # Enables mouse warping to center of newly focused window
   focus-follows-mouse._props = {
-    max-scroll-amount = "5%";
-  };
+    max-scroll-amount = "5%"; # Won't focus a window if it will result in the view scrolling more than the set amount. The value is a percentage of the working area width.
+  }; # Focuses windows and outputs automatically when moving the mouse over them.
+  workspace-auto-back-and-forth = []; # With this, switching to the same workspace by index twice will switch back to the previous workspace.
 }
 // (hostVars.niri.input or {})
