6c48693fac5b4787586e5fe57ded95c29d3741bc
skifli <121291719+skifli@users.noreply.github.com>
2026-08-10 16:10:15 +0100

    feat(kde): fix qt styling + feat(hm): add a bunch more user scripts (incl. init `4-niri.sh`) + feat(niri): add pride (:3) logo

---
+++ users/ami/scripts/colour-picker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PICKED=$(niri msg pick-color 2>/dev/null || true)
+if [ -n "$PICKED" ]; then
+    HEX=$(echo "$PICKED" | grep -i "Hex:" | awk '{print $2}')
+    if [ -n "$HEX" ]; then
+        echo -n "$HEX" | wl-copy
+        notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 3000 "Color Picked" "$HEX (copied to clipboard)"
+    fi
+fi
