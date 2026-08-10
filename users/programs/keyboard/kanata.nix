49fc0c119a12b38ea1d3efce50d492cc69a4dc77
skifli <121291719+skifli@users.noreply.github.com>
2026-08-10 18:23:42 +0100

    feat(kanata): init config

---
+++ users/programs/keyboard/kanata.nix
@@ -0,0 +1,12 @@
+{ pkgs, userVars, ... }: {
+
+  # Enable Kanata system service
+  services.kanata = {
+    enable = true;
+    keyboards = userVars.kanata.keyboards;
+  };
+
+  hardware.uinput.enable = true;
+  users.groups.uinput.members = [ userVars.username ];
+  users.groups.input.members = [ userVars.username ];
+}
