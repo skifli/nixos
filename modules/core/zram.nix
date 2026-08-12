2dda90fe64cab4658ad66d6a19d0691b8e299982
skifli <121291719+skifli@users.noreply.github.com>
2026-08-12 17:18:43 +0100

    fix: try reduce sys lag during rebuilds

--- modules/core/zram.nix
+++ modules/core/zram.nix
@@ -1,6 +1,12 @@
 _: {
   zramSwap = {
     enable = true;
-    memoryPercent = 50;
+    memoryPercent = 125;
+    # Compression algorithm.
+    # lzo has good compression, but is slow.
+    # lz4 has bad compression, but is fast.
+    # zstd is both good compression and fast, but requires newer kernel.
+    # You can check what other algorithms are supported by your zram device with cat /sys/class/block/zram*/comp_algorithm
+    algorithm = "zstd";
   };
 }
