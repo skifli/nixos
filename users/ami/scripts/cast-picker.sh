139b87964923c434b4abb2298ac17741014316fe
skifli <121291719+skifli@users.noreply.github.com>
2026-08-14 19:48:16 +0100

    fix(cast-picker.sh): capitalisation of `notify-send` cmds

--- users/ami/scripts/cast-picker.sh
+++ users/ami/scripts/cast-picker.sh
@@ -5,12 +5,12 @@ if [ "$ACTION" = "window" ]; then
     WIN_ID=$(niri msg --json pick-window 2>/dev/null | jq -r '.id // empty')
     if [ -n "$WIN_ID" ]; then
         niri msg action set-dynamic-cast-window --id "$WIN_ID"
-        notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 2500 "Screencast Target" "Window ID $WIN_ID set as cast target"
+        notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 2500 "Screencast target" "Window ID $WIN_ID set as cast target"
     fi
 elif [ "$ACTION" = "monitor" ]; then
     niri msg action set-dynamic-cast-monitor
-    notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 2500 "Screencast Target" "Focused monitor set as cast target"
+    notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 2500 "Screencast target" "Focused monitor set as cast target"
 elif [ "$ACTION" = "clear" ]; then
     niri msg action clear-dynamic-cast-target
-    notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 2500 "Screencast Target" "Screencast target cleared"
+    notify-send -e -a niri -i "/home/${USER}/.local/share/misc/niri-icon.svg" -u low -t 2500 "Screencast target" "Cleared successfully"
 fi
