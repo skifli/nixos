426836b474a167125593d8bd60728f5a24375a85
skifli <121291719+skifli@users.noreply.github.com>
2026-08-15 22:06:21 +0000

    chore(fmt): run alejandra

--- users/programs/misc/ydotool.nix
+++ users/programs/misc/ydotool.nix
@@ -5,5 +5,5 @@
 }: {
   programs.ydotool.enable = true; # Whether to enable ydotoold system service and ydotool for members of programs.ydotool.group.
 
-  users.users.${userVars.username}.extraGroups = [ "ydotool" ];
+  users.users.${userVars.username}.extraGroups = ["ydotool"];
 }
