8cd0f373e3854f548003fcb24b11716e7300d68c
skifli <121291719+skifli@users.noreply.github.com>
2026-08-15 12:03:51 +0100

    feat(niri): add `schedule.sh` and `task-receiver.sh` and bind them

---
+++ users/ami/scripts/schedule.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Inspired by axlefublr
+
+FIFO_PATH="$HOME/.local/share/mine/task-scheduler"
+mkdir -p "$(dirname "$FIFO_PATH")"
+[ -p "$FIFO_PATH" ] || mkfifo "$FIFO_PATH"
+
+if [ $# -eq 0 ]; then
+    echo "Usage: schedule <command...>"
+    exit 1
+fi
+
+# Write CWD\0COMMAND\0 into the FIFO
+printf "%s\0%s\0" "$PWD" "$*" > "$FIFO_PATH"
+
+notify-send -e -a "Task Scheduler" -i "$HOME/.local/share/misc/nix-snowflake-rainbow.svg" -u low -t 2500 "Task queued" "$*"
