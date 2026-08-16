8ad7a630170dba1ed48369dc93c5e08ba9f4b889
skifli <121291719+skifli@users.noreply.github.com>
2026-08-16 09:50:33 +0000

    chore(fmt): run alejandra

--- users/programs/misc/styles.nix
+++ users/programs/misc/styles.nix
@@ -15,7 +15,6 @@
   13/08/2026@13:31 - Finally fixed for good - Anytype switches to system theme when it changes without a restart! I THINK (not 100% sure) it was linked to this commit - https://github.com/skifli/nixos/commit/4ec0c216570e677e6ec6d5a4d1d1d083e7dceb2a. Specifically, setting `org.freedesktop.impl.portal.Settings` to just `gtk`. At least this charade is all over now, phew!
   13/08/2026@22:22 - It broke again but after a rebuild with ADW_DISABLE_PORTAL=1 it works again??? I didn't know before, I definitely don't know now. Just, if I restart my PC tomorrow, and it's switched - I'll be a happy blob of existence. Anyway this hurts my brain, goodnight :sob:.
   */
-
   latVal = toString (
     if hostVars.latitude >= 0
     then hostVars.latitude
