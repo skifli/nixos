4bc6a998fd8933c3e03ca4cbcb172ecb56751a88
skifli <121291719+skifli@users.noreply.github.com>
2026-08-16 20:33:25 +0000

    chore(fmt): run alejandra

--- users/programs/nightlight/sunsetr.nix
+++ users/programs/nightlight/sunsetr.nix
@@ -11,18 +11,18 @@
     ];
 
     home.activation.setSunsetrCoordinates = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  TARGET_FILE="$HOME/.config/sunsetr/sunsetr.toml"
-  mkdir -p "$(dirname "$TARGET_FILE")"
+        TARGET_FILE="$HOME/.config/sunsetr/sunsetr.toml"
+        mkdir -p "$(dirname "$TARGET_FILE")"
 
-  if [ ! -f "$TARGET_FILE" ]; then
-    cat << 'EOF' > "$TARGET_FILE"
-latitude = ${toString hostVars.latitude}
-longitude = ${toString hostVars.longitude}
-EOF
-  else
-    sed -i 's/^latitude.*/latitude = ${toString hostVars.latitude}/' "$TARGET_FILE"
-    sed -i 's/^longitude.*/longitude = ${toString hostVars.longitude}/' "$TARGET_FILE"
-  fi
-'';
+        if [ ! -f "$TARGET_FILE" ]; then
+          cat << 'EOF' > "$TARGET_FILE"
+      latitude = ${toString hostVars.latitude}
+      longitude = ${toString hostVars.longitude}
+      EOF
+        else
+          sed -i 's/^latitude.*/latitude = ${toString hostVars.latitude}/' "$TARGET_FILE"
+          sed -i 's/^longitude.*/longitude = ${toString hostVars.longitude}/' "$TARGET_FILE"
+        fi
+    '';
   };
 }
