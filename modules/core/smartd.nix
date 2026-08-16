cd24c30efa1785c4986df6d11ad96c7b459b0752
skifli <121291719+skifli@users.noreply.github.com>
2026-08-16 22:27:34 +0100

    fix(smartd): forgot to add changes

--- modules/core/smartd.nix
+++ modules/core/smartd.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  hostVars,
+  pkgs,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     smartmontools # provides `smartctl`
     gsmartcontrol # provides the GTK GUI inspector
@@ -6,16 +10,9 @@
 
   services.smartd = {
     enable = true;
-    autodetect = true; # Automatically monitors NVMe, SSDs, and HDDs
+    autodetect = false; # Explicitly defined below
 
-    # Custom smartd rules applied to all detected drives:
-    # -a: Monitor all SMART attributes
-    # -o on: Enable automatic SMART background testing
-    # -S on: Enable automatic saving of SMART attributes
-    # -n standby,q: Do NOT wake if sleeping/spun-down
-    # -s (S/../.././02|L/../../6/03): Short test daily at 2:00 AM, Long test every Saturday at 3:00 AM
-    # -W 4,70,80: Temperature alerts: Report 4°C delta, warn at 70°C, critical at 80°C (for NVMe)
-    defaults.autodetected = "-a -o on -S on -n standby,q -s (S/../.././02|L/../../6/03) -W 4,70,80";
+    devices = hostVars.devices;
 
     notifications = {
       wall.enable = false; # Disable terminal-wide broadcasting
