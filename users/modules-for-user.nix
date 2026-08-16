a3ced6e3310a70fd6b71c17e66a7d2a97947dfc4
skifli <121291719+skifli@users.noreply.github.com>
2026-08-16 12:32:40 +0100

    feat(modules-for-user.nix): optimise by culling calls to `builtins.pathExists`

--- users/modules-for-user.nix
+++ users/modules-for-user.nix
@@ -5,47 +5,35 @@
 } @ attrs: let
   inherit (userVars) programs;
 
-  requireModulePath = kind: path:
-    if builtins.pathExists path
-    then path
-    else throw "Missing ${kind} module for user '${userVars.username}': ${toString path}";
-
-  categoryDirFor = category:
-    if builtins.pathExists (./programs/${category})
-    then category
-    else if lib.hasSuffix "s" category && builtins.pathExists (./programs/${lib.removeSuffix "s" category})
-    then lib.removeSuffix "s" category
-    else category;
-
-  mkProgramModulePath = category: name:
-    requireModulePath "program" ./programs/${categoryDirFor category}/${name}.nix;
+  # Map plural categories to singular directory names
+  categoryDir = cat: # Support ones where multiple packages can be installed from a single category, e.g. "browsers" to "browser"
+    if cat == "browsers" then "browser"
+    else cat;
 
   regular = lib.concatMap (
     category: let
       value = programs.${category};
+      dir = categoryDir category;
     in
       if category == "other"
       then []
       else if builtins.isList value
-      then map (name: mkProgramModulePath category name) value
+      then map (name: ./programs/${dir}/${name}.nix) value
       else if value != ""
-      then [(mkProgramModulePath category value)]
+      then [./programs/${dir}/${value}.nix]
       else []
   ) (builtins.attrNames programs); # Get all programs specified in the usual way
 
-  others = map (program: requireModulePath "other" ./programs/misc/${program}.nix) (programs.other or []); # Get all programs specified in the "other" variable
+  others = map (program: ./programs/misc/${program}.nix) (programs.other or []); # Get all programs specified in the "other" variable
 
   all =
     [
-      (requireModulePath "user-packages" ./${userVars.username}/user-packages.nix)
+      ./${userVars.username}/user-packages.nix
     ]
     ++ lib.optional userVars.git.enabled ./programs/misc/git.nix
     ++ regular
     ++ others;
-
-  # Wrap each module so it receives expected args (userVars, pkgs, etc.)
-  wrap = path: import path attrs; # attrs captured the entire argument set, including not explicitly listed keys
 in
-  map wrap all
-# Combine all programs into one big list
-
+  # Wrap each module so it receives expected args (userVars, pkgs, etc.)
+  map (path: import path attrs) all # attrs captured the entire argument set, including not explicitly listed keys
+  # Combine all programs into one big list
