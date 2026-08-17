2a6a0904f040ade4b693fb1d848f7bf44452eecb
skifli <121291719+skifli@users.noreply.github.com>
2026-08-17 10:27:17 +0100

    feat(workflows): make less strenuous + general cleanups

--- users/programs/ergonomics/safeeyes.nix
+++ users/programs/ergonomics/safeeyes.nix
@@ -13,6 +13,10 @@
             python3Packages.croniter # Needed for Health Statistics
             python3Packages.pywayland # Needed for Smart Pause
           ];
+        propagatedBuildInputs = (oldAttrs.propagatedBuildInputs or []) ++ [
+            pkgs.python3Packages.croniter
+            pkgs.python3Packages.pywayland
+        ]; # Depending on the nixpkgs channel version overriding by just dependencies may not propagate so do both
       }))
     ];
 
