2257c2e4efd6fee40963dc94912a53287eb71e35
skifli <121291719+skifli@users.noreply.github.com>
2026-08-26 17:40:19 +0100

    fix(kde-polkit-agent-1): re-add (whoops!)

--- users/programs/display-server/wayland.nix
+++ users/programs/display-server/wayland.nix
@@ -24,14 +24,32 @@
     slurp # https://wiki.archlinux.org/title/XDG_Desktop_Portal#Using_multiple_monitors_with_xdg-desktop-portal-wlr
     seahorse # Managing gnome keyring secrets
     libsecret # For testing & needed for polling gcr-prompter in startupScript
+    kdePackages.polkit-kde-agent-1 # Polkit stuff
   ];
 
   # Enable XWayland support system-wide
   programs.xwayland.enable = true;
 
-  # Try fix some gnome-keyring oddities
-  systemd.services."autovt@tty1".enable = false;
-  systemd.services."getty@tty1".enable = false;
+  systemd = {
+    # Try fix some gnome-keyring oddities
+    services."autovt@tty1".enable = false;
+    services."getty@tty1".enable = false;
+
+    # Start the agent as a graphical user service
+    user.services.polkit-kde-agent-1 = {
+      description = "polkit-kde-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
 
   # Enable systemd user session support
   services = {
