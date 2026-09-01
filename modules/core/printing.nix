aa537623c00c728c990e2483113dc815922cef48
skifli <121291719+skifli@users.noreply.github.com>
2026-09-01 17:26:33 +0100

    feat(printing.nix): add SANE support

--- modules/core/printing.nix
+++ modules/core/printing.nix
@@ -26,6 +26,12 @@
     };
   };
 
+  # Enable SANE scanner support + driverless AirScan/WSD backend
+  hardware.sane = {
+    enable = true;
+    extraBackends = [pkgs.sane-airscan];
+  };
+
   hardware.printers = lib.mkIf ((hostVars.printers or []) != []) {
     ensurePrinters = hostVars.printers;
     ensureDefaultPrinter = (builtins.head hostVars.printers).name; # Default printer defaults to first printer
@@ -34,5 +40,14 @@
   environment.systemPackages = with pkgs; [
     system-config-printer
     cups-filters
+
+    # Scanner utilities and GUI
+    sane-backends # Provides scanimage, sane-find-scanner CLI tools
+    sane-airscan # CLI discovery tool (airscan-discover)
+    kdePackages.skanpage # KDE
   ];
+
+  users.users = lib.genAttrs hostVars.enabledUsers (username: {
+    extraGroups = ["lp" "scanner"];
+  });
 }
