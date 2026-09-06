476f5d7cc4a962249e0bbc42b3bd6e75c08a18af
skifli <121291719+skifli@users.noreply.github.com>
2026-09-06 16:14:30 +0100

    fix(fydetabduo): disable breakages due to plymouth/`kernelParams`

--- hosts/fydetabduo/configuration.nix
+++ hosts/fydetabduo/configuration.nix
@@ -47,22 +47,9 @@ in
     };
   };
 
-  boot.loader.fydetabduo.enable = true;
-
   # Do NOT import modules/core/boot.nix - for the following reason:
   # that module also selects Lyra's kernel and bootloader configuration.
-  boot = {
-    plymouth = {
-      enable = true;
-      theme = "breeze";
-    };
-    kernelParams = [
-      "splash"
-      "systemd.show_status=false"
-      "rd.systemd.show_status=false"
-      "vt.global_cursor_default=0"
-    ];
-  };
+  boot.loader.fydetabduo.enable = true;
 
   # NFS mounts over tailscale (with hard/timeo=600) leave processes D-state
   # when suspend closes the tunnel; systemd-sleep then waits a full ~60s
