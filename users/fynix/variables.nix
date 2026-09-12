0aea34c90917285662eec8521f2a3da9e584b6c9
skifli <121291719+skifli@users.noreply.github.com>
2026-09-12 20:14:00 +0100

    fix(fynix): update user variables

--- users/fynix/variables.nix
+++ users/fynix/variables.nix
@@ -306,7 +306,7 @@ common
   programs = common.programs // {
     login-manager = "tuigreet";
     terminal = "foot";
-    other = common.programs.other ++ [ "qwarp" ];
+    other = common.programs.other ++ [ "qwarp" "rnote" ];
   };
 
   stylixTargetsWhitelist = common.stylixTargetsWhitelist ++ [ programs.terminal ];
